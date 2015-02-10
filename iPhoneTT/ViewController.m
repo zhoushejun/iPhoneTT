df9f7eefce30b2301465521bfb004ccd97f739df
shejun.zhou <shejunzhou@shejunzhoudeMacBook-Pro.local>
2015-02-10 19:17:46 +0800

    完善获取App在当前哪种设备环境下运行功能

--- iPhoneTT/ViewController.m
+++ iPhoneTT/ViewController.m
@@ -7,6 +7,14 @@
 //
 
 #import "ViewController.h"
+#import "YBDeviceManager.h"
+
+/** @name 获取屏幕 宽度、高度 及 状态栏 高度 */
+// @{
+#define SCREEN_WIDTH  ([UIScreen mainScreen].bounds.size.width)
+#define SCREEN_HEIGHT ([UIScreen mainScreen].bounds.size.height)
+// @}end of 获取屏幕 宽度、高度 及 状态栏 高度
+
 
 @interface ViewController ()
 
@@ -17,11 +25,25 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn.frame = CGRectMake(10, 100, SCREEN_WIDTH - 20, 44);
+    btn.backgroundColor = [UIColor grayColor];
+    [btn setTitle:@"test" forState:UIControlStateNormal];
+    btn.titleLabel.textColor = [UIColor orangeColor];
+    btn.titleLabel.textAlignment = NSTextAlignmentCenter;
+    [btn addTarget:self action:@selector(btnShowDeviceVersion:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)btnShowDeviceVersion:(id)sender {
+    NSString *strDevicePlatform = [NSString stringWithFormat:@"%@",[YBDeviceManager devicePlatform]];
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"" message:strDevicePlatform delegate:nil cancelButtonTitle:@"OK" otherButtonTitles: nil];
+    [alertView show];
 }
 
 @end
---
+++ iPhoneTT/YBDeviceManager.m
@@ -0,0 +1,136 @@
+//
+//  YBDeviceManager.m
+//  iPhoneTT
+//
+//  Created by shejun.zhou on 15/2/9.
+//  Copyright (c) 2015年 YiBan.iOS. All rights reserved.
+//
+
+#import "YBDeviceManager.h"
+#import "sys/utsname.h"
+
+@implementation YBDeviceManager
+
++ (NSString *)devicePlatform {
+    static NSString *platform;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        platform = [self platformString];
+    });
+    return platform;
+}
+
++ (NSString *)deviceVersion {
+    struct utsname systemInfo;
+    uname(&systemInfo);
+    NSString *deviceString = [NSString stringWithCString:systemInfo.machine encoding:NSUTF8StringEncoding];
+    
+    return deviceString;
+}
+
++ (NSString *)platformString {
+    
+    NSString *platform = [self deviceVersion];
+    
+    //iPhone
+    
+    if ([platform isEqualToString:@"iPhone1,1"]) return @"iPhone 1";
+    
+    if ([platform isEqualToString:@"iPhone1,2"]) return @"iPhone 3";
+    
+    if ([platform isEqualToString:@"iPhone2,1"]) return @"iPhone 3GS";
+    
+    if ([platform isEqualToString:@"iPhone3,1"]) return @"iPhone 4";
+    
+    if ([platform isEqualToString:@"iPhone3,2"]) return @"iPhone 4";
+    
+    if ([platform isEqualToString:@"iPhone3,3"]) return @"iPhone 4";
+    
+    if ([platform isEqualToString:@"iPhone4,1"]) return @"iPhone 4s";
+    
+    if ([platform isEqualToString:@"iPhone5,1"]) return @"iPhone 5";
+    
+    if ([platform isEqualToString:@"iPhone5,2"]) return @"iPhone 5";
+    
+    if ([platform isEqualToString:@"iPhone5,3"]) return @"iPhone 5C";
+    
+    if ([platform isEqualToString:@"iPhone5,4"]) return @"iPhone 5C";
+    
+    if ([platform isEqualToString:@"iPhone6,1"]) return @"iPhone 5S";
+    
+    if ([platform isEqualToString:@"iPhone6,2"]) return @"iPhone 5S";
+    
+    if ([platform isEqualToString:@"iPhone7,1"]) return @"iPhone 6";
+    
+    if ([platform isEqualToString:@"iPhone7,2"]) return @"iPhone 6 Plus";
+    
+    //iPot Touch
+    
+    if ([platform isEqualToString:@"iPod1,1"]) return @"iPod Touch";
+    
+    if ([platform isEqualToString:@"iPod2,1"]) return @"iPod Touch 2";
+    
+    if ([platform isEqualToString:@"iPod3,1"]) return @"iPod Touch 3";
+    
+    if ([platform isEqualToString:@"iPod4,1"]) return @"iPod Touch 4";
+    
+    if ([platform isEqualToString:@"iPod5,1"]) return @"iPod Touch 5";
+    
+    //iPad
+    
+    if ([platform isEqualToString:@"iPad1,1"]) return @"iPad";
+    
+    if ([platform isEqualToString:@"iPad2,1"]) return @"iPad 2";
+    
+    if ([platform isEqualToString:@"iPad2,2"]) return @"iPad 2";
+    
+    if ([platform isEqualToString:@"iPad2,3"]) return @"iPad 2";
+    
+    if ([platform isEqualToString:@"iPad2,4"]) return @"iPad 2";
+    
+    if ([platform isEqualToString:@"iPad2,5"]) return @"iPad Mini 1";
+    
+    if ([platform isEqualToString:@"iPad2,6"]) return @"iPad Mini 1";
+    
+    if ([platform isEqualToString:@"iPad2,7"]) return @"iPad Mini 1";
+    
+    if ([platform isEqualToString:@"iPad3,1"]) return @"iPad 3";
+    
+    if ([platform isEqualToString:@"iPad3,2"]) return @"iPad 3";
+    
+    if ([platform isEqualToString:@"iPad3,3"]) return @"iPad 3";
+    
+    if ([platform isEqualToString:@"iPad3,4"]) return @"iPad 4";
+    
+    if ([platform isEqualToString:@"iPad3,5"]) return @"iPad 4";
+    
+    if ([platform isEqualToString:@"iPad3,6"]) return @"iPad 4";
+    
+    if ([platform isEqualToString:@"iPad4,1"]) return @"iPad air";
+    
+    if ([platform isEqualToString:@"iPad4,2"]) return @"iPad air";
+    
+    if ([platform isEqualToString:@"iPad4,3"]) return @"iPad air";
+    
+    if ([platform isEqualToString:@"iPad4,4"]) return @"iPad mini 2";
+    
+    if ([platform isEqualToString:@"iPad4,5"]) return @"iPad mini 2";
+    
+    if ([platform isEqualToString:@"iPad4,6"]) return @"iPad mini 2";
+    
+    if ([platform isEqualToString:@"iPad4,7"]) return @"iPad mini 3";
+    
+    if ([platform isEqualToString:@"iPad4,8"]) return @"iPad mini 3";
+    
+    if ([platform isEqualToString:@"iPad4,9"]) return @"iPad mini 3";
+    
+    if ([platform isEqualToString:@"iPad5,3"]) return @"iPad air 2";
+    
+    if ([platform isEqualToString:@"iPad5,4"]) return @"iPad air 2";
+    
+    if ([platform isEqualToString:@"iPhone Simulator"] || [platform isEqualToString:@"x86_64"]) return @"iPhone Simulator";
+    
+    return platform;
+}
+
+@end
